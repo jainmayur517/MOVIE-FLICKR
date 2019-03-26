f817d988d1fdd23912ca08fc65aa111d7a001a17
jainmayur517 <jainmayur517@gmail.com>
2019-03-27 00:44:47 +0530

    v2.0

--- index.js
+++ index.js
@@ -9,15 +9,32 @@ app.get("/",function(req,res){
 res.render("search");
 })
 app.get("/results",function(req,res){
+    
 var name=req.query.bm
 request("https://api.themoviedb.org/3/search/movie?api_key=4d3d897644294d2ef0d6db5feff11716&query=" + name,function(error,response,body){
 
 if(!error && response.statusCode==200){
 var result=JSON.parse(body);
+//console.log(result)
 res.render("app", {data: result});
 }
 });
 });
+
+app.get("/results/:id",function(req,res){
+    //console.log(req.params.id);
+    var name=req.params.id;
+    request("http://api.themoviedb.org/3/movie/"+name+"/casts?api_key=4d3d897644294d2ef0d6db5feff11716",function(error,response,body){
+        var data = JSON.parse(body)
+        res.render("cast", {data:data})
+        /*if(!error && response.statusCode==200){
+            var data=JSON.parse(body);
+            res.render("cast", {datadata});
+            console.log("C")   
+    }*/
+    });
+    });
+
 app.get("/popularity",function(req,res){
 request("https://api.themoviedb.org/3/discover/movie?api_key=4d3d897644294d2ef0d6db5feff11716&sort_by=popularity.desc",function(error,response,body){
     if(!error && response.statusCode==200){
@@ -55,19 +72,33 @@ app.get("/resultstv",function(req,res){
     request("http://api.themoviedb.org/3/tv/top_rated?api_key=4d3d897644294d2ef0d6db5feff11716",function(error,response,body){
         if(!error && response.statusCode==200){
             var data=JSON.parse(body);
-            res.render("toprated", {data});   
+            res.render("topratedtv", {data});   
     }
     });
     });
     
-app.get("/topratedtv",function(req,res){
-        request("http://api.themoviedb.org/3/tv/top_rated?api_key=4d3d897644294d2ef0d6db5feff11716",function(error,response,body){
+app.get("/populartv",function(req,res){
+        request("https://api.themoviedb.org/3/discover/tv?api_key=4d3d897644294d2ef0d6db5feff11716&sort_by=popularity.desc",function(error,response,body){
             if(!error && response.statusCode==200){
                 var data=JSON.parse(body);
-                res.render("toprated", {data});   
+                res.render("populartv", {data});   
         }
+        console.log("hello")
         });
         });   
+
+app.get("/moviecast",function(req,res){
+   var name=req.query.movieid;
+            request("http://api.themoviedb.org/3/movie/"+name+"/casts?api_key=4d3d897644294d2ef0d6db5feff11716",function(error,response,body){
+                if(!error && response.statusCode==200){
+                    var data=JSON.parse(body);
+                    res.render("cast", {data:data});   
+            }
+            });
+            });          
+
+
+       
 
 
 
